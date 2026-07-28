ea4f5d846df294db5369df3061d3a618ba447092
naadensua-01 <narteynaadensuaotoo@gmail.com>
2026-07-28 07:28:44 +0000

    my final work

--- assignment_06_multiplication_table.py
+++ assignment_06_multiplication_table.py
@@ -55,3 +55,36 @@
 # YOUR CODE BELOW — remove the # symbols from the scaffold and fill it in
 # =============================================================================
 
+def single_table(number):
+    print(f"Multiplication Table for {number}: ")
+
+    for i in range(1, 13):
+        print(f"{number} x {i} = {number * i}")
+
+#Part B
+def multiple_tables(n):
+    for number in range (1, n + 1):
+        print(f"Multiplication Table for {number}:")
+
+        for i in range(1, 13):
+            print(f"{number} x {i} = {number * i}")
+            
+        print("---------------------------")
+
+
+# Part A input
+num = int(input("Enter a number for multiplication table: "))
+
+if num <= 0:
+    print("Error: Number must be a positive integer.")
+else:
+    single_table(num)
+
+
+# Part B input
+n = int(input("\nEnter N to print tables from 1 to N: "))
+
+if n <= 0:
+    print("Error: N must be a positive integer.")
+else:
+    multiple_tables(n)
